1d24feb12f5d6d7ee29c72429feafd45675441e1
Ksenia <ksenia.briling@epfl.ch>
2023-10-20 09:28:52 +0200

    Assign reaction classes based on changing bonds

--- run.sh
+++ run.sh
@@ -1,11 +1,12 @@
 wandb enabled
 wandb disabled
 python train.py --device='cuda' \
-                --experiment_name='run-gpu' \
+                --experiment_name='run-gpu2' \
                 --subset 100 \
-                --num_epochs=3 \
-                --process \
+                --num_epochs=2 \
                 --dataset cyclo \
                 --atom_mapping \
-                #--noH \
+                --noH \
+#                --checkpoint logs/run-gpu/231006-135410.579171-xe.best_checkpoint.pt \
                 #--rxnmapper \
+                #--process \
