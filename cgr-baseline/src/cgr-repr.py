3325855269e5e6241b047acee1ec6a78a5fecf97
Ksenia <ksenia.briling@epfl.ch>
2023-12-11 11:32:18 +0100

    Refactor aee28efb5; reproducibility problem?

--- cgr-baseline/src/cgr-repr.py
+++ cgr-baseline/src/cgr-repr.py
@@ -8,25 +8,33 @@ import tqdm
 import chemprop
 from chemprop.features.featurization import set_reaction
 
-col = 'rxn_smiles_mapped'  # change if change columns
+
+parser = ap.ArgumentParser()
+parser.add_argument('--column', default='rxn_smiles_mapped', help='csv file column to use, should be the same as used for training')
+parser.add_argument('--reaction_mode', default='reac_diff', help='should be the same as used for training, do not change')
+parser.add_argument('--checkpoint', default='fold_0/model_0/model.pt', help='path to the checkpoint')
+parser.add_argument('--prediction', action='store_true', help='predict targets')
+parser.add_argument('--representation', action='store_true', help='save representations')
+parser.add_argument('--mpn_path', default='MPN.npy', help='path to save the output of the MPNN portion of the model')
+parser.add_argument('--last_ffn_path', default='last_FFN', help='path to save the the input to the final readout layer')
+args = parser.parse_args()
 
 arguments = [
     '--test_path', '/dev/null',
     '--preds_path', '/dev/null',
-    '--checkpoint_path', 'fold_00/model_0/model.pt'
+    '--checkpoint_path', args.checkpoint,
 ]
 
-args = chemprop.args.PredictArgs().parse_args(arguments)
+chemprop_args = chemprop.args.PredictArgs().parse_args(arguments)
 
-set_reaction(True, 'reac_diff')  # because if first creates the model and then reads its parameters
-model_objects = chemprop.train.load_model(args=args)
+set_reaction(True, args.reaction_mode)  # because if first creates the model and then reads its parameters
+model_objects = chemprop.train.load_model(args=chemprop_args)
 tr_args, pred_args, models, scalers, tasks, names = model_objects
 
-print(tr_args)
 df = pd.read_csv(tr_args.data_path, index_col=0)
-smiles = df[col].to_numpy()
+smiles = df[args.column].to_numpy()
 
-if True:
+if args.representation:
     x = []
     y = []
     tr_args, pred_args, models, scalers, tasks, names = model_objects
@@ -40,7 +48,7 @@ if True:
     np.save('MPN.npy', x)
     np.save('last_FFN.npy', y)
 
-if True:
+if args.prediction:
     for smi in tqdm.tqdm(smiles):
-        z = chemprop.train.make_predictions(args=args, smiles=[[smi]], model_objects=model_objects)
-        print(z)
+        p = chemprop.train.make_predictions(args=chemprop_args, smiles=[[smi]], model_objects=model_objects)
+        print(p)
