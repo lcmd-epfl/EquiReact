437caee26bfd0e1e2c8383923bae8d3f590a0ee2
Ksenia <ksenia.briling@epfl.ch>
2024-04-14 01:45:44 +0200

    Update EquiReact results
    TODO: add by_mol, representation for tsne, etc

--- results/get-results-from-wandb.py
+++ results/get-results-from-wandb.py
@@ -8,22 +8,25 @@ import wandb
 
 api = wandb.Api()
 
-for dataset in ('gdb', 'proparg', 'cyclo'):
+for dataset in ('proparg', 'cyclo', 'gdb'):
 
-    runs = api.runs(f'nequireact-{dataset}')
+    runs = api.runs(f'nequireact-{dataset}-80')
     results = defaultdict(list)
 
     for run in runs:
-        if run.name.startswith('cv10-LP'):
-            test = run.summary['test_score']
-            print(run.id, run.name, run.config['seed'], test)
-            results[run.name].append(test)
+        if run.name.startswith('cv10-'):
+            test_score = run.summary['test_score']
+            test_rmse  = run.summary['test_rmse']
+            print(run.id, run.name, run.config['seed'], test_score, test_rmse)
+            results[run.name].append((test_score, test_rmse))
     print()
 
     for i, ri in results.items():
         if len(ri)!=10:
             warnings.warn(f"set '{i}' contains {len(ri)} runs")
         ri = np.array(ri)
-        print(i, np.mean(ri), np.std(ri))
+        test_mean = np.mean(ri, axis=0)
+        test_std  = np.std(ri, axis=0)
+        print(i, test_mean[0], test_std[0], test_mean[1], test_std[1])
     print()
     print()
