497719ed3b40a71869c5b157314d512c62c88147
Ksenia <ksenia.briling@epfl.ch>
2024-05-06 21:55:11 +0200

    Fix a bad bug: use products and reactants

--- data-curation/compare_cyclo_dft_xtb.py
+++ data-curation/compare_cyclo_dft_xtb.py
@@ -10,13 +10,14 @@ df = pd.read_csv(f'{data_dir}/cyclo.csv', index_col=0)
 bad_idx = np.loadtxt(f'{data_dir}/bad-xtb.dat', dtype=int)
 for idx in bad_idx:
     df.drop(df[df['rxn_id']==idx].index, axis=0, inplace=True)
-indices = df['rxn_id'].values
 
-for idx in indices:
+for idx, switch in zip(df['rxn_id'], df['switch_reactants']):
 
     pfile_dft  = glob(f'{data_dir}/xyz/{idx}/p*.xyz')[0]
     r0file_dft = sorted(glob(f'{data_dir}/xyz/{idx}/r1*.xyz'))[-1]
     r1file_dft = sorted(glob(f'{data_dir}/xyz/{idx}/r0*.xyz'))[-1]
+    if switch:
+        r0file_dft, r1file_dft = r1file_dft, r0file_dft
     pfile_xtb  = f'{data_dir}/xyz-xtb/Product_{idx}.xyz'
     r0file_xtb = f'{data_dir}/xyz-xtb/Reactant_{idx}_0.xyz'
     r1file_xtb = f'{data_dir}/xyz-xtb/Reactant_{idx}_1.xyz'
@@ -25,10 +26,12 @@ for idx in indices:
     xtb_files = [pfile_xtb, r0file_xtb, r1file_xtb]
 
     diff = 0.0
-    for x, y in zip(dft_files, xtb_files):
-        m1 = ase.io.read(pfile_dft)
-        m2 = ase.io.read(pfile_xtb)
-        diff += np.linalg.norm(m1.positions-m1.get_center_of_mass()-m2.positions+m2.get_center_of_mass())**2
+    for dft, xtb in zip(dft_files, xtb_files):
+        mol_dft = ase.io.read(dft)
+        mol_xtb = ase.io.read(xtb)
+        r_dft = mol_dft.positions-mol_dft.get_center_of_mass()
+        r_xtb = mol_xtb.positions-mol_xtb.get_center_of_mass()
+        diff += np.linalg.norm(r_dft-r_xtb)**2
     m = ase.io.read(pfile_dft)
     N = m.get_global_number_of_atoms()
     n = np.count_nonzero(m.get_atomic_numbers()>1)
